c7c59762f918a5c93f416b97d9d1e9890318ed42
NB-N13 <nb@n13.media>
2022-08-23 09:42:46 +0200

    N13-custom-arrow-0.1

---
+++ N13-custom-arrow.js
@@ -0,0 +1,9 @@
+<script>
+$('[n13-slider-arrow]').on('click', function() {
+  var sliderarrowid = $(this).attr('n13-slider-arrow');
+  var wrapperid = sliderarrowid.match(/\d+/);
+  var wrapperattribute = '[n13-custom-slider-arrow='+wrapperid+']';
+  var ogarrowid = '[n13-og-slider-arrow='+sliderarrowid+']';
+  $(wrapperattribute).find(ogarrowid).click();
+});
+</script>
